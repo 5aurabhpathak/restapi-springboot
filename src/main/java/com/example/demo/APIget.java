a399e723c618819f1d71796cc61201f6b22bc4fd
5aurabhpathak <s.pathak@ieee.org>
2018-03-21 03:09:11 +0530

    init

---
+++ src/main/java/com/example/demo/APIget.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class APIget {
+
+    private final int register;
+    
+    public APIget(int register) {
+        this.register = register;
+    }
+
+    public int getContent() {
+        return register;
+    }
+}
---
+++ src/main/java/com/example/demo/APIpost.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class APIpost {
+
+    private final int register;
+    private final long value;
+    
+    public APIpost(int register, long value) {
+        this.register = register;
+        this.value = value;
+    }
+}
