b6e0c8ff0ccde5431d6715849ea60b2cb03841c8
5aurabhpathak <s.pathak@ieee.org>
2018-03-22 05:49:51 +0530

    update2

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,11 +1,17 @@
 package com.example.demo;
 
+import java.util.HashMap;
+
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,45 +19,71 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 public class Controller {
 	
-	private APIpost p;
-
-    @PostMapping("/post")
-    public void post(@RequestBody APIpost postobj) {
-    	p = postobj;
-    	System.out.println("Register:" + p.register);
-    	System.out.println("Value:" + p.value);
+	@PatchMapping("/devices/{deviceid}")
+    @ResponseBody
+    public ResponseEntity<?> patch(@PathVariable("deviceid") int devId, @Valid @RequestBody API patchobj) {
+    	try {
+	    	Device device = DeviceList.deviceList.get(devId);
+	    	int regId = patchobj.getRegisterId();
+	    	HashMap<Integer, Integer> registers = device.Registers();
+        	if (registers.containsKey(regId)) registers.put(regId, patchobj.getValue());
+        	else throw new NullPointerException();
+	    	return new ResponseEntity<Device>(device, HttpStatus.OK);
+	    }
+    	catch (NullPointerException e) {
+    		return new ResponseEntity<Status>(new Status("Failed"), HttpStatus.NOT_FOUND);
+    	}
     }
     
-    @PatchMapping("/post")
+    @PostMapping("/devices/{deviceid}/{register}")
     @ResponseBody
-    public ResponseEntity patch(@RequestBody APIpost patchobj) {
+    public ResponseEntity<Status> post(@PathVariable("deviceid") int devId, @PathVariable("register") int regId, @Valid @RequestBody API postobj) {
     	try {
-	    	p.register = (patchobj.register != p.register && patchobj.register != 0) ? patchobj.register : p.register;
-	    	p.value = (patchobj.value != p.value && patchobj.value != 0) ? patchobj.value : p.value;
-	    	System.out.println("Register:" + p.register);
-	    	System.out.println("Value:" + p.value);
-	    	return new ResponseEntity<APIpost>(p, HttpStatus.OK);
+    		Device device = DeviceList.deviceList.get(devId);
+    		HashMap<Integer, Integer> registers = device.Registers();
+        	if (registers.containsKey(regId)) registers.put(regId, postobj.getValue());
+        	else throw new NullPointerException();
+        	return new ResponseEntity<Status>(new Status("Success"), HttpStatus.OK); 
     	}
     	catch (NullPointerException e) {
-    		return new ResponseEntity<Status>(new Status("Error: Nothing to Patch!"), HttpStatus.NOT_FOUND);
+    		return new ResponseEntity<Status>(new Status("Failed"), HttpStatus.NOT_FOUND);
     	}
     }
     
-    @GetMapping("/{deviceid}")
+    @GetMapping("/devices/{deviceid}")
     @ResponseBody
-    public APIget get(@PathVariable("deviceid") int devId) {
-    	return new APIget(123, devId);
+    public ResponseEntity<?> get(@PathVariable("deviceid") int devId) {
+    	try {
+    		if (!DeviceList.deviceList.containsKey(devId)) throw new NullPointerException();
+    		return new ResponseEntity<Device>(DeviceList.deviceList.get(devId), HttpStatus.OK);
+    	}
+    	catch (NullPointerException e) {
+    		return new ResponseEntity<Status>(new Status("Device not found"), HttpStatus.NOT_FOUND);
+    	}
     }
-}
-
-class Status {
-	private String msg;
-	
-	Status(String msg) {
-		this.msg = msg;
-	}
-	
-	public String getStatus( ) {
-		return msg;
-	}
+    
+    @DeleteMapping("/devices/{deviceid}")
+    @ResponseBody
+    public ResponseEntity<Status> del(@PathVariable("deviceid") int devId) {
+    	try {
+			DeviceList.deviceList.remove(devId);
+        	return new ResponseEntity<Status>(new Status("Success"), HttpStatus.OK);
+    	}
+    	catch (NullPointerException e) {
+    		return new ResponseEntity<Status>(new Status("Failed: Device already not present"), HttpStatus.NOT_FOUND);
+    	}
+    }
+    
+    @GetMapping("/devices")
+    @ResponseBody
+    public ResponseEntity<DeviceList> get() {
+    	return new ResponseEntity<DeviceList>(new DeviceList(), HttpStatus.OK);
+    }
+    
+    @PutMapping("/devices")
+    @ResponseBody
+    public ResponseEntity<Status> put(@Valid @RequestBody Device deviceobj) {
+		DeviceList.deviceList.put(deviceobj.getId(), deviceobj);
+        return new ResponseEntity<Status>(new Status("Success"), HttpStatus.OK);
+    }
 }
